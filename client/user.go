211efdce8ce415fa72af99402e3e9d38d71d4652
vsaveliev <savelievharlam@gmail.com>
2017-05-15 23:07:58 +0700

    added client package

---
+++ client/user.go
@@ -0,0 +1,33 @@
+package client
+
+const (
+	userUrlStr = "/user"
+)
+
+type UserService struct {
+	client *Client
+}
+
+func (u *UserService) Sync(user User) error {
+	req, err := u.client.NewRequest(postMethod, userUrlStr, u)
+	if err != nil {
+		return err
+	}
+
+	_, err = u.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type User struct {
+	Name *string `json:"name"`
+}
+
+func NewUser(username string) *User {
+	return &User{
+		Name: &username,
+	}
+}
